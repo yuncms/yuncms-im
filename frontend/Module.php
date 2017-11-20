7d81069e844faa33a321c4c18e617f30f5b46853
Peter <xutongle@gmail.com>
2017-11-20 18:49:40 +0800

    init

---
+++ frontend/Module.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @link http://www.tintsoft.com/
+ * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
+ * @license http://www.tintsoft.com/license/
+ */
+
+namespace yuncms\im\frontend;
+
+/**
+ * Class Module
+ * @package yuncms\im
+ */
+class Module extends \yuncms\im\Module
+{
+
+}
