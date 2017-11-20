54dfb83bf7b5145fa7fb6e11ef9cdfca61f65152
Peter <xutongle@gmail.com>
2017-11-20 18:37:00 +0800

    任务监听

---
+++ Bootstrap.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @link http://www.tintsoft.com/
+ * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
+ * @license http://www.tintsoft.com/license/
+ */
+namespace yuncms\im;
+
+use Yii;
+use yii\base\Event;
+use yii\base\BootstrapInterface;
+use yuncms\core\helpers\ShortURL;
+use yuncms\user\models\User;
+use xutl\tim\ImJob;
+
+/**
+ * Class Bootstrap
+ * @package yuncms\im
+ */
+class Bootstrap implements BootstrapInterface
+{
+    /**
+     * @inheritdoc
+     */
+    public function bootstrap($app)
+    {
+        Event::on(User::className(), User::AFTER_CREATE, function ($event) {
+            Yii::$app->queue->push(new ImJob([
+                'action' => ImJob::ACTION_ACCOUNT_CREATE,
+                'params' => [
+                    'Identifier' => ShortURL::encode($event->sender->id),
+                    'Nick' => $event->sender->username,
+                    //'FaceUrl'
+                ],
+            ]));
+        });
+    }
+}
