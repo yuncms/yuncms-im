f1721c76bdcad922ac9f44bb9177ea67d4956470
xutongle <xutongle@gmail.com>
2018-01-26 20:37:07 +0800

    test

--- Bootstrap.php
+++ Bootstrap.php
@@ -4,6 +4,7 @@
  * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
  * @license http://www.tintsoft.com/license/
  */
+
 namespace yuncms\im;
 
 use Yii;
@@ -11,7 +12,6 @@ use yii\base\Event;
 use yii\base\BootstrapInterface;
 use yuncms\core\helpers\ShortURL;
 use yuncms\user\models\User;
-use xutl\tim\ImJob;
 
 /**
  * Class Bootstrap
@@ -26,26 +26,12 @@ class Bootstrap implements BootstrapInterface
     {
         //监听用户注册事件，自动激活IM
         Event::on(User::className(), User::AFTER_CREATE, function ($event) {
-            Yii::$app->queue->push(new ImJob([
-                'action' => ImJob::ACTION_ACCOUNT_CREATE,
-                'params' => [
-                    'Identifier' => ShortURL::encode($event->sender->id),
-                    'Nick' => $event->sender->nickname,
-                    //'FaceUrl'
-                ],
-            ]));
+            Yii::$app->im->getAccount()->import($event->sender->id, $event->sender->nickname);
         });
 
         //监听用户注册事件，自动激活IM
         Event::on(User::className(), User::AFTER_REGISTER, function ($event) {
-            Yii::$app->queue->push(new ImJob([
-                'action' => ImJob::ACTION_ACCOUNT_CREATE,
-                'params' => [
-                    'Identifier' => ShortURL::encode($event->sender->id),
-                    'Nick' => $event->sender->nickname,
-                    //'FaceUrl'
-                ],
-            ]));
+            Yii::$app->im->getAccount()->import($event->sender->id, $event->sender->nickname);
         });
     }
 }
